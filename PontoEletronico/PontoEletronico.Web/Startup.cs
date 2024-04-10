c28697444b170e0ef5d8087ddf6d46d07299c016
Wanderleyps <wanderleyps794@gmail.com>
2024-04-10 15:49:20 -0300

    implementação dos repositorios

--- PontoEletronico/PontoEletronico.Web/Startup.cs
+++ PontoEletronico/PontoEletronico.Web/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PontoEletronico.Infra.IoC;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PontoEletronico.Web
 {
@@ -23,6 +19,7 @@ namespace PontoEletronico.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure(Configuration);
             services.AddControllersWithViews();
         }
 
