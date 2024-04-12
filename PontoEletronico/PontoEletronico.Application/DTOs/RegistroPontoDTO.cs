4b96cd60bc65a95f8f59d50d73f526756eb09de5
Wanderleyps <wanderleyps794@gmail.com>
2024-04-12 12:14:40 -0300

    Implementação da listagem de pontos por funcionário

--- PontoEletronico/PontoEletronico.Application/DTOs/RegistroPontoDTO.cs
+++ PontoEletronico/PontoEletronico.Application/DTOs/RegistroPontoDTO.cs
@@ -1,4 +1,5 @@
-﻿using PontoEletronico.Domain.Enums;
+﻿using PontoEletronico.Domain.Entities;
+using PontoEletronico.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -18,8 +19,7 @@ namespace PontoEletronico.Application.DTOs
         public DateTime Data { get; set; }
 
         public TimeSpan Hora { get; set; }
-
-        [Required(ErrorMessage = "O ID do funcionário é obrigatório.")]
+        
         public int FuncionarioId { get; set; }
 
         public string Erro { get; set; }    
--- PontoEletronico/PontoEletronico.Web/Controllers/RegistroPontoController.cs
+++ PontoEletronico/PontoEletronico.Web/Controllers/RegistroPontoController.cs
@@ -1,26 +1,46 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PontoEletronico.Application.DTOs;
 using PontoEletronico.Application.Interfaces;
+using PontoEletronico.Application.Services;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PontoEletronico.Web.Controllers
 {
     public class RegistroPontoController : Controller
     {
 
-        private readonly IRegistroPontoService _registopontoService;
+        private readonly IRegistroPontoService _registoPontoService;
+        private readonly IFuncionarioService _funcionarioService;
         private readonly IWebHostEnvironment _environment;
 
-        public RegistroPontoController(IRegistroPontoService registroPontoService, IWebHostEnvironment environment)
+        public RegistroPontoController(IRegistroPontoService registroPontoService, 
+                                       IFuncionarioService funcionarioService, 
+                                       IWebHostEnvironment environment)
         {
-            _registopontoService = registroPontoService;
+            _registoPontoService = registroPontoService;
+            _funcionarioService = funcionarioService;
             _environment = environment;
         }
 
         // GET: RegistroPontoController
-        public ActionResult Index(int? id)
+        public async Task<IActionResult> Index(int? funcionarioId)
         {
-            return View();
+            if (funcionarioId == null) return NotFound();
+
+            var registoPontos = await _registoPontoService.GetByFuncionarioIdAsync(funcionarioId.GetValueOrDefault());
+            
+            if (registoPontos == null) return NotFound();
+
+            var funcionarioDTO = await _funcionarioService.GetByIdAsync(funcionarioId.GetValueOrDefault());
+
+            if (funcionarioDTO == null) return NotFound();
+
+            ViewBag.Funcionario = funcionarioDTO;
+
+            return View(registoPontos);
         }
 
         // GET: RegistroPontoController/Details/5
